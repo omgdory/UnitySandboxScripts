e7d794ceffc5e8ab95dca87e9fb0d712515f1e24
Dorian A <91112526+omgdory@users.noreply.github.com>
2023-07-29 09:50:15 -0700

    Add: RacingMenuManager
    Add RacingMenuManager and organized files

--- CarSandbox/TextHandler.cs
+++ CarSandbox/TextHandler.cs
@@ -5,21 +5,21 @@ using TMPro;
 
 public class TextHandler : MonoBehaviour
 {
+
+    // Reference to player
+    [SerializeField] private GameObject player;
+    private Rigidbody rbPlayer;
     // Reference to CarManager script (for upsideDown variable)
     private CarManager carManager;
 
-    // Reference to player
-    private GameObject player;
-    private Rigidbody rbPlayer;
-
     // Vars for handling UI
     [SerializeField] private GameObject canvas;
-    private GameObject SpeedTextMeshObject;
-    private GameObject FlipPromptTextMeshObject;
-    private GameObject InfoTextMeshObject;
-    private GameObject ToggleInfoButtonObject;
+    [SerializeField] private GameObject SpeedTextMeshObject;
+    [SerializeField] private GameObject FlipPromptTextMeshObject;
+    [SerializeField] private GameObject InfoTextMeshObject;
+    [SerializeField] private GameObject ToggleInfoButtonObject;
     private double currSpeed;
-    private double speedConvert = 1.5;
+    private double speedConversionFactor = 1.5;
 
     // Gotten from CarManager script (external reference)
     private bool upsideDown;
@@ -28,19 +28,11 @@ public class TextHandler : MonoBehaviour
         if(!canvas.activeSelf) {
             canvas.SetActive(true);
         }
-        
-        // Find appropriate objects
-        canvas = GameObject.Find("Canvas");
-        SpeedTextMeshObject = GameObject.Find("Canvas/UI_Speed");
-        FlipPromptTextMeshObject = GameObject.Find("Canvas/UI_FlipPrompt");
-        InfoTextMeshObject = GameObject.Find("Canvas/UI_Info");
-        ToggleInfoButtonObject = GameObject.Find("Canvas/ToggleInfoButton");
 
         // Disable flip prompt
         FlipPromptTextMeshObject.SetActive(false);
 
         // Set up player variables
-        player = GameObject.Find("PLAYER");
         carManager = player.GetComponent<CarManager>();
         rbPlayer = player.GetComponent<Rigidbody>();
     }
@@ -48,18 +40,15 @@ public class TextHandler : MonoBehaviour
     void FixedUpdate() {
         upsideDown = carManager.upsideDown;
 
-        currSpeed = GetSpeed() * speedConvert;
+        currSpeed = GetSpeedActualSpeed() * speedConversionFactor;
         SpeedTextMeshObject.GetComponent<TextMeshProUGUI>().text = $"Speed: {currSpeed.ToString("F1")} mph";
 
         HandleUpsideDown();
     }
-
-    private double GetSpeed() {
+ 
+    private double GetSpeedActualSpeed() {
         // Object reference not set to an instance of an object
-        double result = rbPlayer.velocity.magnitude;
-        if(result <= 0.1)
-            return 0.0;
-        return result;
+        return rbPlayer.velocity.magnitude;
     }
 
     private void HandleUpsideDown() {
---
+++ RacingMenu/RacingMenuManager.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RacingMenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject carPrefab_Purple;
+    [SerializeField] private Transform display;
+    private GameObject displayCar;
+    private Vector3 displayCarOffset_pos;
+    private Vector3 displayCarOffset_rot;
+
+    [SerializeField] private GameObject creditsSubtitle;
+
+    [SerializeField] private GameObject playButton;
+    [SerializeField] private GameObject settingsButton;
+    [SerializeField] private GameObject creditsButton;
+    [SerializeField] private GameObject quitButton;
+
+    [SerializeField] private float rotateDisplaySpeed;
+    [SerializeField] private bool rotateDisplayClockwise;
+
+    void Start()
+    {
+        displayCarOffset_pos = display.transform.position;
+        displayCarOffset_rot = new Vector3(0,90,0);
+        CreateDisplayCar(displayCarOffset_pos, displayCarOffset_rot);
+
+        creditsSubtitle.SetActive(false);
+    }
+
+    void FixedUpdate()
+    {
+        RotateTransform(displayCar.transform, rotateDisplaySpeed, rotateDisplayClockwise);
+    }
+
+    /* Function to rotate a transform
+        @param obj Transform to rotate
+        @param speed Speed at which to rotate
+        @param clockwise Whether or not it will rotate clockwise relative to the global y axis.
+        Set to true by default. */
+    private void RotateTransform(Transform obj, float speed, bool clockwise = true) {
+        float cw = clockwise ? 1.0f : -1.0f;
+        Quaternion targetRotation = obj.rotation * Quaternion.Euler(0, 1.0f * cw , 0);
+        obj.rotation = Quaternion.Lerp(obj.rotation, targetRotation, speed * Time.deltaTime);
+    }
+
+    private void CreateDisplayCar(Vector3 positionOffset, Vector3 rotationOffset) {
+        if(displayCar != null) {
+            Destroy(displayCar);
+        }
+        // https://discussions.unity.com/t/how-can-i-instantiate-a-gameobject-directly-into-another-gameobject-as-child/86294
+        displayCar = Instantiate(carPrefab_Purple);
+        displayCar.transform.parent = display;
+
+        displayCar.transform.position += positionOffset;
+        displayCar.transform.rotation *= Quaternion.Euler(rotationOffset);
+    }
+
+    public void onPlayButton() {
+        // Start game -- go to next scene in build index
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    public void onSettingsButton() {
+        // TO DO
+    }
+
+    public void onCreditsButton() {
+        if(creditsSubtitle.activeSelf) {
+            creditsSubtitle.SetActive(false);
+            return;
+        }
+        creditsSubtitle.SetActive(true);
+    }
+
+    public void onQuitButton() {
+        Application.Quit();
+    }
+}
